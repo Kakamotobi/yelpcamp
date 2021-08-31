e3b9e40451a47583b38a605998f8b0586cd5fcb6
Kakamotobi <daeram.chung@gmail.com>
2021-08-31 20:18:41 +0900

    Add edit campround

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground.js");
 
 const app = express();
@@ -23,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // -----Middleware----- //
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // -----Routes----- //
 app.get("/", (req, res) => {
@@ -50,6 +52,20 @@ app.post("/campgrounds", async (req, res) => {
 app.get("/campgrounds/:id", async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render("campgrounds/show.ejs", { campground });
+});
+
+// Serve form to edit campground
+app.get("/campgrounds/:id/edit", async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render("campgrounds/edit.ejs", { campground });
+});
+// Submit form to edit campground
+app.put("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, {
+		...req.body.campground,
+	});
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 // -----Port----- //
