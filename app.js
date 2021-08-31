7bd7059e4439470d8264f82bd6bd6b4cbf7501f4
Kakamotobi <daeram.chung@gmail.com>
2021-08-31 19:15:33 +0900

    Setup routes for campgrounds

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get("/campgrounds", async (req, res) => {
 	res.render("campgrounds/index.ejs", { campgrounds });
 });
 
-// Campground Detail
+// Campground Details
 app.get("/campgrounds/:id", async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render("campgrounds/show.ejs", { campground });
